70b628b305109a83869f658c78dec2817f7fef16
aymen-ghemam <gh.aymen39@gmail.com>
2023-01-17 22:31:39 +0100

    fix responsivity

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(elm => {
         elm.target.classList.toggle('show', elm.isIntersecting);
-        if(elm.isIntersecting) observer.unobserve(elm.target);
+        // if(elm.isIntersecting) observer.unobserve(elm.target);
     })
 }, {
-    threshold: 0.3,
+    threshold: 0.1,
 })
 
 document.querySelectorAll('section').forEach(elm => {
